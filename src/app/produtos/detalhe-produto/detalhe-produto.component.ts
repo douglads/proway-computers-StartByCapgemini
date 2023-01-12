2d95ae44a92322cbf8877f69117f6db277106571
Douglads <douglasfm1995@gmail.com>
2023-01-12 00:31:16 -0300

    6-H: Desenvolvimento parte lógica, página de produto

--- src/app/produtos/detalhe-produto/detalhe-produto.component.ts
+++ src/app/produtos/detalhe-produto/detalhe-produto.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { IProduto } from 'src/app/produtos';
+import { ProdutosService } from 'src/app/produtos.service';
 
 @Component({
   selector: 'app-detalhe-produto',
   templateUrl: './detalhe-produto.component.html',
   styleUrls: ['./detalhe-produto.component.css']
 })
-export class DetalheProdutoComponent {
-
+export class DetalheProdutoComponent implements OnInit{
+  produto : IProduto | undefined;
+  quantidade : Number = 1;
+    
+  constructor(private produtosService : ProdutosService,
+    private route : ActivatedRoute){}
+  ngOnInit(): void {
+    const routeParams = this.route.snapshot.paramMap;
+    const produtoId = Number(routeParams.get("id"));
+    this.produto = this.produtosService.getOne(produtoId);
+  }
 }
